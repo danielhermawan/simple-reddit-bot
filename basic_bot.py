2a6c6a772a3a22fb042f3c6aafb3ef3203d15408
Daniel Hermawan <danielhermawan96@gmail.com>
2017-08-09 18:36:00 +0700

    Add Basic Bot

---
+++ basic_bot.py
@@ -0,0 +1,36 @@
+import praw
+import os
+import re
+import pdb
+import logging
+
+
+def log_request():
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.DEBUG)
+    logger = logging.getLogger('prawcore')
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+
+def main():
+    log_request()
+    reddit = praw.Reddit('bot1')
+    if not os.path.isfile('posts_replied_to.txt'):
+        posts_replied_to = []
+    else:
+        with open('posts_replied_to.txt', 'r') as f:
+            posts_replied_to = f.read().split("\n")
+            posts_replied_to = list(filter(None, posts_replied_to))
+    subreddit = reddit.subreddit('pythonforengineers')
+    for submission in subreddit.hot(limit=5):
+        if submission.id not in posts_replied_to:
+            if re.search("i love python", submission.title, re.IGNORECASE):
+                submission.reply("Botty bot says: Me too!!")
+                posts_replied_to.append(submission.id)
+                with open("posts_replied_to.txt", "w") as f:
+                    for post_id in posts_replied_to:
+                        f.write(post_id + "\n")
+
+
+if __name__ == '__main__':
+    main()
--- bot.py
+++
@@ -1,6 +0,0 @@
-import praw
-
-reddit = praw.Reddit('bot1')
-
-for submission in reddit.subreddit('learnpython').hot(limit=10):
-    print(submission.title)
